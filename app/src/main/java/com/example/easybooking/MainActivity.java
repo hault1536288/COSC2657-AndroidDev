362bbae23e9b732baa953e0c88a1889e1dfafff8
khue <khue251103@gmail.com>
2024-12-30 14:38:32 +0700

    testing bottom navigation, create placeholder fragments

--- app/src/main/java/com/example/easybooking/MainActivity.java
+++ app/src/main/java/com/example/easybooking/MainActivity.java
@@ -7,18 +7,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.easybooking.fragments.BookingFragment;
+import com.example.easybooking.fragments.CarFragment;
+import com.example.easybooking.fragments.HotelFragment;
+import com.example.easybooking.fragments.ProfileFragment;
+import com.example.easybooking.fragments.TransitFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+            if (itemId == R.id.hotel) {
+                replaceFragment(new HotelFragment());
+            } else if (itemId == R.id.car) {
+                replaceFragment(new CarFragment());
+            } else if (itemId == R.id.transit) {
+                replaceFragment(new TransitFragment());
+            } else if (itemId == R.id.booking) {
+                replaceFragment(new BookingFragment());
+            } else if (itemId == R.id.profile) {
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
         });
+        replaceFragment(new HotelFragment()); // Set inital fragment to Hotel
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, fragment);
+        fragmentTransaction.commit();
     }
 }
