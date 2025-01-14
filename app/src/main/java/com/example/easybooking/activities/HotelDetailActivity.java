e39a2dd934000fee92a3ff686f6d1999f3e902b8
khue <khue251103@gmail.com>
2025-01-14 17:54:04 +0700

    finish sync hotel data

--- app/src/main/java/com/example/easybooking/activities/HotelDetailActivity.java
+++ app/src/main/java/com/example/easybooking/activities/HotelDetailActivity.java
@@ -1,14 +1,13 @@
 package com.example.easybooking.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.bumptech.glide.Glide;
 import com.example.easybooking.R;
@@ -25,22 +24,31 @@ public class HotelDetailActivity extends AppCompatActivity {
         String hotelName = getIntent().getStringExtra("HOTEL_NAME");
         String hotelDescription = getIntent().getStringExtra("HOTEL_DESCRIPTION");
         String hotelLocation = getIntent().getStringExtra("HOTEL_LOCATION");
-        double hotelRating = getIntent().getDoubleExtra("HOTEL_RATING", 0.0);
+        double latitude = getIntent().getDoubleExtra("HOTEL_LAT", 0.0);
+        double longitude = getIntent().getDoubleExtra("HOTEL_LNG", 0.0);
 
         ImageView hotelDetailImageView = findViewById(R.id.hotelDetailImageView);
         TextView hotelDetailNameTextView = findViewById(R.id.hotelDetailNameTextView);
-        TextView hotelDetailRatingTextView = findViewById(R.id.hotelDetailRatingTextView);
         TextView hotelDetailDescriptionTextView = findViewById(R.id.hotelDetailDescriptionTextView);
         TextView hotelDetailLocationTextView = findViewById(R.id.hotelDetailLocationTextView);
+        Button viewOnMapButton = findViewById(R.id.viewOnMapButton);
 
         Glide.with(this)
                 .load(hotelImageUrl)
                 .placeholder(R.drawable.ic_hotel)
                 .error(R.drawable.ic_hotel)
                 .into(hotelDetailImageView);
+
         hotelDetailNameTextView.setText(hotelName);
-        hotelDetailRatingTextView.setText(String.valueOf(hotelRating));
         hotelDetailDescriptionTextView.setText(hotelDescription);
         hotelDetailLocationTextView.setText(hotelLocation);
+
+        viewOnMapButton.setOnClickListener(v -> {
+            Intent intent = new Intent(HotelDetailActivity.this, MapActivity.class);
+            intent.putExtra("NAME", hotelName);
+            intent.putExtra("LAT", latitude);
+            intent.putExtra("LNG", longitude);
+            startActivity(intent);
+        });
     }
 }
